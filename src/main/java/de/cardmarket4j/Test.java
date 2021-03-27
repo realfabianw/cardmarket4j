9b20ff4b795dbb902223bcebbe77dfc9cde7fb71
Fabian <32239266+realfabianw@users.noreply.github.com>
2021-03-27 16:49:44 +0100

    Create Test.java

---
+++ src/main/java/de/cardmarket4j/Test.java
@@ -0,0 +1,20 @@
+package de.cardmarket4j;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	private static Logger LOGGER = LoggerFactory.getLogger("Test");
+
+	public static void main(String[] args) throws IOException {
+		CardMarketService cardMarketService = new CardMarketService("SgM0KpesvabMCz8q",
+				"TDwfy4yj3HAniBwsYyPcQEgHc0YL0Cgb", "OzbPvdcDJmr4IXYNSPLavmnjQm19SwVO",
+				"liWmJqYiMEl9q3QWoDZBNMpKyUCRsL6A");
+
+		cardMarketService.getAccountService().getMessages();
+
+	}
+
+}
